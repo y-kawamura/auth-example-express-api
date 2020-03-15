100653926fb1567cb92154152bd092180001a1a8
y-kawamura <tetora.y@gmail.com>
2020-03-15 15:51:50 +0900

    Check if username is unique

--- auth/index.js
+++ auth/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const Joi = require('@hapi/joi');
 
+const db = require('../db/connection');
+const users = db.get('users');
+users.createIndex('username', { unique: true });
+
 const router = express.Router();
 
 const schema = Joi.object({
@@ -19,9 +23,30 @@ router.get('/', (req, res) => {
   });
 });
 
-router.post('/signup', (req, res) => {
-  const result = schema.validate(req.body);
-  res.json(result);  
+router.post('/signup', async (req, res, next) => {
+  try {
+    const result = await schema.validateAsync(req.body);
+    
+    // make sure username is unique
+    const user = await users.findOne({
+      username: result.username
+    });
+    if (user) {
+      console.log(user);
+      
+      // there is already a user in the DB with this username
+      const error = new Error('That username is already exist. Please choose another one.');
+      res.statusCode=400;
+      return next(error);
+    }
+    
+    // hash the password
+
+    // insert to DB
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
 })
 
 module.exports = router;
