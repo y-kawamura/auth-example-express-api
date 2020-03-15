278445d0846ea28e3ab46a21e9d811353b72449f
y-kawamura <tetora.y@gmail.com>
2020-03-15 14:49:57 +0900

    Valdate required fields on signup with @hapi/joi

--- auth/index.js
+++ auth/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const Joi = require('@hapi/joi');
 
 const router = express.Router();
+
+const schema = Joi.object({
+  username: Joi.string()
+      .alphanum()
+      .min(2)
+      .max(30)
+      .required(),
+  password: Joi.string()
+      .pattern(new RegExp('^[a-zA-Z0-9-_]{3,}$')),
+});
 
 router.get('/', (req, res) => {
   res.json({
@@ -9,10 +20,8 @@ router.get('/', (req, res) => {
 });
 
 router.post('/signup', (req, res) => {
-  console.log(req.body);
-  res.json({
-    message: 'signup'
-  });  
+  const result = schema.validate(req.body);
+  res.json(result);  
 })
 
 module.exports = router;
