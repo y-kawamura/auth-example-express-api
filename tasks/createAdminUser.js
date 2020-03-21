24a267e62a5ba953f360b3f891bced928ae60963
y-kawamura <tetora.y@gmail.com>
2020-03-21 14:34:29 +0900

    Seed admin user

---
+++ tasks/createAdminUser.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const db = require('../db/connection');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
+const users = db.get('users');
+
+async function createAdminUser() {
+  try {
+    const user = await users.findOne({ role: 'admin' });
+    if (user) {
+      console.log('Admin user already eixsts');
+    } else {
+      await users.insert({
+        username: 'Admin',
+        password: await bcrypt.hash(process.env.DEFAULT_ADMIN_PASSWORD, saltRounds),
+        role: 'admin',
+        active: true,
+      });
+      console.log('Admin user created');
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    db.close();
+  }
+}
+
+createAdminUser();
