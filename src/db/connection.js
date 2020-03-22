af6c95ae80c628d1334655057f426a0c739c6888
y-kawamura <tetora.y@gmail.com>
2020-03-22 14:19:09 +0900

    Change env key to MONGODB_URL

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,7 @@ const monk = require('monk');
 
 const dbUrl = process.env.NODE_ENV === 'test'
   ? process.env.TEST_DB_URL
-  : process.env.DB_URL;
+  : process.env.MONGODB_URI;
 
 const db = monk(dbUrl);
 
